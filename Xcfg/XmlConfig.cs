323fee3a34ab2b6e17e5c5213b27b6f5bbc0f31c
shuqizhao <shuqizhao@qq.com>
2017-11-20 20:32:26 +0800

    fix null

--- Xcfg/XmlConfig.cs
+++ Xcfg/XmlConfig.cs
@@ -52,7 +52,7 @@ namespace Xcfg
 
         public static T GetRemoteConfigSectionParam(string cfgName)
         {
-            
+            return default(T);
         }
 
         [XmlAttribute("major")]
